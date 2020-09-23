aed18de99d0184a37dfac7a7c0cdc81a98aca44f
Aleksey Markelov <rcusstackwalker@yandex.ru>
2020-09-23 15:15:01 +0500

    transition to CMake

--- reject.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#gcc codeinjector.c -L/home/main/binutils-2.22/bfd -l bfd -l /home/main/binutils-2.22/libiberty/libiberty.a -l z -o codeinjector
-gcc -g main.c supported_ecus.c -l bfd -l dl -o codeinjector
